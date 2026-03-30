8cc962a3f2e49695b06b2f71c74dac770c36b881
Andrew Gameworks <wuveilphantom@gmail.com>
2026-03-30 11:53:43 +0700

    Update config.js

--- api/config.js
+++ api/config.js
@@ -7,15 +7,20 @@ export default function handler(req, res) {
   const { id } = req.query;
 
   // Data User (Nanti bisa dipindah ke Database Supabase/MongoDB)
-  const users = {
+const users = {
     "andrewsmile": {
       username: "andrewsmile",
       theme: "dark",
       buttonText: "Follow me on Instagram",
       profilePic: "https://unavatar.io/instagram/andrewsmile"
+    },
+    "skyrabbitsgames": {
+      username: "skyrabbitsgames",
+      theme: "dark",
+      buttonText: "Play with Skyrabbits!",
+      profilePic: "https://unavatar.io/instagram/skyrabbitsgames"
     }
   };
-
   const config = users[id] || users["andrewsmile"];
 
   res.status(200).json(config);
